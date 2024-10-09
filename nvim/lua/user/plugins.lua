02d5e503df118f845eab84615c35d72fc19b1c34
Nick Stebunov <mailbox@stebunoff.ru>
2024-10-09 11:48:44 +0300

    Nvim: terminals

--- nvim/lua/user/plugins.lua
+++ nvim/lua/user/plugins.lua
@@ -186,6 +186,24 @@ use({
   end,
 })
 
+--- Floating terminal.
+use({
+  'voldikss/vim-floaterm',
+  config = function()
+    vim.g.floaterm_width = 0.8
+    -- vim.g.floaterm_height = 0.8
+    -- Settings for terminal in the bottom
+    vim.g.floaterm_height = 0.4
+    vim.g.floaterm_wintype = 'split'
+    vim.keymap.set('n', '<F1>', ':FloatermToggle<CR>')
+    vim.keymap.set('t', '<F1>', '<C-\\><C-n>:FloatermToggle<CR>')
+    vim.cmd([[
+      highlight link Floaterm CursorLine
+      highlight link FloatermBorder CursorLineBg
+    ]])
+  end
+})
+
 -- Automatically set up your configuration after cloning packer.nvim
 -- Put this at the end after all plugins
 if packer_bootstrap then
