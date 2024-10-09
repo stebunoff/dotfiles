abe8dd19cd45d7e32304913d62c22954552b0e5a
Nick Stebunov <mailbox@stebunoff.ru>
2024-10-09 10:59:24 +0300

    Nvim: plugin code fixes

--- nvim/lua/user/plugins/dashboard-nvim.lua
+++ nvim/lua/user/plugins/dashboard-nvim.lua
@@ -1,20 +1,30 @@
 local dashboard = require('dashboard')
-dashboard.custom_header = {
-  '',
-  '',
-  '  ⢸⣦⡈⠻⣿⣿⣿⣶⣄',
-  '    ⢸⣿⣿⣦⡈⠻⣿⣿⣿⣷⣄',
-  '⣀⣀⣀⣀⣀⣀⣼⣿⣿⣿⣿ ⠈⠻⣿⣿⣿⣷⣄',
-  '  ⠈⠻⣿⣿⣿⣿⣿⡿⠿⠛⠁   ⠈⠻⢿⣿⣿⣷⣄',
-  '',
+
+dashboard.setup {
+  config = {
+    header = {
+      '',
+    	-- your ASCII art here wrapped every line in single quotes
+      '',
+    },
+    center = {
+      { icon = '  ', desc = 'New file                       ', action = 'enew' },
+      { icon = '  ', shortcut = 'SPC f', desc = 'Find file                 ', action = 'Telescope find_files' },
+      { icon = '  ', shortcut = 'SPC h', desc = 'Recent files              ', action = 'Telescope oldfiles' },
+      { icon = '  ', shortcut = 'SPC g', desc = 'Find Word                 ', action = 'Telescope live_grep' },
+    },
+    footer = {}
+  }
 }
-dashboard.custom_center = {
-  { icon = '  ', desc = 'New file                       ', action = 'enew' },
-  { icon = '  ', shortcut = 'SPC f', desc = 'Find file                 ', action = 'Telescope find_files' },
-  { icon = '  ', shortcut = 'SPC h', desc = 'Recent files              ', action = 'Telescope oldfiles' },
-  { icon = '  ', shortcut = 'SPC g', desc = 'Find Word                 ', action = 'Telescope live_grep' },
-}
-dashboard.custom_footer = { '' }
+
+vim.cmd([[
+  augroup DashboardHighlights
+    autocmd ColorScheme * highlight DashboardHeader guifg=#6272a4
+    autocmd ColorScheme * highlight DashboardCenter guifg=#f8f8f2
+    autocmd ColorScheme * highlight DashboardShortcut guifg=#bd93f9
+    autocmd ColorScheme * highlight DashboardFooter guifg=#6272a4
+  augroup end
+]])
 vim.api.nvim_set_hl(0, 'DashboardHeader', { fg = '#6272a4' })
 vim.api.nvim_set_hl(0, 'DashboardCenter', { fg = '#f8f8f2' })
 vim.api.nvim_set_hl(0, 'DashboardShortcut', { fg = '#bd93f9' })
--- nvim/lua/user/plugins/lualine.lua
+++ nvim/lua/user/plugins/lualine.lua
@@ -21,7 +21,7 @@ require('lualine').setup({
       'branch',
       'diff',
       separator,
-      '"🖧  " .. tostring(#vim.tbl_keys(vim.lsp.buf_get_clients()))',
+      '"💻  " .. tostring(#vim.tbl_keys(vim.lsp.buf_get_clients()))',
       { 'diagnostics', sources = { 'nvim_diagnostic' } },
       separator,
     },
