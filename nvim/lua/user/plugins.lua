0be069fa2484d523e15c6c8a9c453e526fa44563
Nick Stebunov <mailbox@stebunoff.ru>
2024-10-10 09:31:33 +0300

    Nvim: linting

--- nvim/lua/user/plugins.lua
+++ nvim/lua/user/plugins.lua
@@ -227,6 +227,8 @@ use({
     'williamboman/mason.nvim',
     'williamboman/mason-lspconfig.nvim',
     'b0o/schemastore.nvim',
+    { 'jose-elias-alvarez/null-ls.nvim', dependencies = 'nvim-lua/plenary.nvim' },
+    'jayp0521/mason-null-ls.nvim',
   },
   config = function()
     require('user/plugins/lspconfig')
--- nvim/lua/user/plugins/lspconfig.lua
+++ nvim/lua/user/plugins/lspconfig.lua
@@ -8,21 +8,57 @@ require('mason-lspconfig').setup({
   }
 })
 
-local capabilities = require("cmp_nvim_lsp").default_capabilities()
+local capabilities = require('cmp_nvim_lsp').default_capabilities(vim.lsp.protocol.make_client_capabilities())
 
 -- PHP
-require('lspconfig').intelephense.setup({ capabilities = capabilities })
+require('lspconfig').intelephense.setup({
+  commands = {
+    IntelephenseIndex = {
+      function()
+        vim.lsp.buf.execute_command({ command = 'intelephense.index.workspace' })
+      end,
+    },
+  },
+  on_attach = function(client, bufnr)
+    client.server_capabilities.documentFormattingProvider = false
+    client.server_capabilities.documentRangeFormattingProvider = false
+  end,
+  capabilities = capabilities
+})
 
 -- Vue, JavaScript, TypeScript
 require('lspconfig').volar.setup({
+  on_attach = function(client, bufnr)
+    client.server_capabilities.documentFormattingProvider = false
+    client.server_capabilities.documentRangeFormattingProvider = false
+  end,
   capabilities = capabilities,
-  -- Enable "Take Over Mode" where volar will provide all JS/TS LSP services
-  -- This drastically improves the responsiveness of diagnostic updates on change
-  filetypes = { 'typescript', 'javascript', 'javascriptreact', 'typescriptreact', 'vue' },
+})
+
+require('lspconfig').ts_ls.setup({
+  init_options = {
+    plugins = {
+      {
+        name = "@vue/typescript-plugin",
+        location = "/usr/local/lib/node_modules/@vue/typescript-plugin",
+        languages = {"javascript", "typescript", "vue"},
+      },
+    },
+  },
+  filetypes = {
+    "javascript",
+    "javascriptreact",
+    "javascript.jsx",
+    "typescript",
+    "typescriptreact",
+    "typescript.tsx",
+    "vue",
+  },
 })
 
 -- Tailwind CSS
 require('lspconfig').tailwindcss.setup({ capabilities = capabilities })
+
 -- JSON
 require('lspconfig').jsonls.setup({
   capabilities = capabilities,
@@ -33,6 +69,48 @@ require('lspconfig').jsonls.setup({
   },
 })
 
+ -- null-ls
+ local null_ls = require('null-ls')
+ local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
+ null_ls.setup({
+   temp_dir = '/tmp',
+   sources = {
+     null_ls.builtins.diagnostics.eslint_d.with({
+       condition = function(utils)
+         return utils.root_has_file({ '.eslintrc.js' })
+       end,
+     }),
+     null_ls.builtins.diagnostics.trail_space.with({ disabled_filetypes = { 'NvimTree' } }),
+     null_ls.builtins.formatting.eslint_d.with({
+       condition = function(utils)
+         return utils.root_has_file({ '.eslintrc.js', '.eslintrc.json' })
+       end,
+     }),
+     null_ls.builtins.formatting.pint.with({
+       condition = function(utils)
+         return utils.root_has_file({ 'vendor/bin/pint' })
+       end,
+     }),
+     null_ls.builtins.formatting.prettier.with({
+       condition = function(utils)
+         return utils.root_has_file({ '.prettierrc', '.prettierrc.json', '.prettierrc.yml', '.prettierrc.js', 'prettier.config.js' })
+       end,
+     }),
+   },
+   on_attach = function(client, bufnr)
+     if client.supports_method("textDocument/formatting") then
+       vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+       vim.api.nvim_create_autocmd("BufWritePre", {
+         group = augroup,
+         buffer = bufnr,
+         callback = function()
+           vim.lsp.buf.format({ bufnr = bufnr, timeout_ms = 5000 })
+         end,
+       })
+     end
+   end,
+ })
+
 -- Keymaps
 vim.keymap.set('n', '<Leader>d', '<cmd>lua vim.diagnostic.open_float()<CR>')
 vim.keymap.set('n', '[d', '<cmd>lua vim.diagnostic.goto_prev()<CR>')
@@ -42,6 +120,7 @@ vim.keymap.set('n', 'gi', ':Telescope lsp_implementations<CR>')
 vim.keymap.set('n', 'gr', ':Telescope lsp_references<CR>')
 vim.keymap.set('n', 'K', '<cmd>lua vim.lsp.buf.hover()<CR>')
 vim.keymap.set('n', '<Leader>rn', '<cmd>lua vim.lsp.buf.rename()<CR>')
+vim.keymap.set('n', 'fo', '<cmd>lua vim.lsp.buf.format()<CR>')
 
 -- Diagnostic configuration
 vim.diagnostic.config({
