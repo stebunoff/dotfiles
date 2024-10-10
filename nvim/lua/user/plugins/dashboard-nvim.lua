dd4a5633f422d1e4cd9ee97ffb9426483daed587
Nick Stebunov <mailbox@stebunoff.ru>
2024-10-10 13:45:19 +0300

    Nvim: cosmetic improvements for status line and dashboard

--- nvim/lua/user/plugins/dashboard-nvim.lua
+++ nvim/lua/user/plugins/dashboard-nvim.lua
@@ -1,31 +1,41 @@
 local dashboard = require('dashboard')
 
 dashboard.setup {
+  theme = 'doom',
   config = {
     header = {
       '',
-    	-- your ASCII art here wrapped every line in single quotes
+      '⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀',
+      '⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣤⡄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣤⣤⣄⠀⠀⣠⣤⣤⠀',
+      '⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣠⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣿⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢰⣿⠋⠉⠉⠀⣿⠟⠉⠉⠀',
+      '⠀⠀⠀⠀⠀⠀⠀⠀⠀⣿⣿⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣿⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣿⣿⠀⠀⠀⢸⣿⠀⠀⠀⠀',
+      '⠀⣾⡿⠿⢿⣷⡄⠀⠿⣿⣿⠿⠿⠀⠀⢀⣾⣿⠿⢿⣷⣄⠀⠀⠀⣿⡇⣤⣾⣿⣿⣦⠀⠀⠀⠀⣿⡇⠀⠀⠀⠀⣿⡇⠀⠀⠀⣿⢀⣴⣿⣿⣷⣄⠀⠀⠀⠀⣠⣶⣿⣿⣷⣤⠀⠀⠀⠿⣿⣿⠿⠿⠿⢿⣿⠿⠿⠇⠀',
+      '⠸⣿⠀⠀⠀⠈⠀⠀⠀⣿⣿⠀⠀⠀⢠⣿⠋⠀⠀⠀⠈⣿⡄⠀⠀⣿⣿⠋⠀⠀⠀⢻⣿⠀⠀⠀⣿⡇⠀⠀⠀⠀⣿⡇⠀⠀⠀⣿⡿⠁⠀⠀⠘⣿⠀⠀⠀⣾⣿⠁⠀⠀⠀⢻⣿⠀⠀⠀⣿⣿⠀⠀⠀⢸⣿⠀⠀⠀⠀',
+      '⠀⠙⠿⣶⣤⣀⠀⠀⠀⣿⣿⠀⠀⠀⢸⣿⠿⠿⠿⠿⠿⠿⠧⠀⠀⣿⡇⠀⠀⠀⠀⠀⣿⡇⠀⠀⣿⡇⠀⠀⠀⠀⣿⡇⠀⠀⠀⣿⠀⠀⠀⠀⠀⣿⡇⠀⠀⣿⠃⠀⠀⠀⠀⠀⣿⡇⠀⠀⣿⣿⠀⠀⠀⢸⣿⠀⠀⠀⠀',
+      '⠀⠀⠀⠀⠈⢿⣧⠀⠀⣿⣿⠀⠀⠀⢸⣿⠀⠀⠀⠀⠀⠀⠀⠀⠀⣿⣇⠀⠀⠀⠀⠀⣿⡇⠀⠀⣿⡇⠀⠀⠀⢀⣿⡇⠀⠀⠀⣿⠀⠀⠀⠀⠀⣿⡇⠀⠀⣿⡆⠀⠀⠀⠀⢀⣿⠇⠀⠀⣿⣿⠀⠀⠀⢸⣿⠀⠀⠀⠀',
+      '⢴⣷⣄⣀⣀⣿⡟⠀⠀⢹⣿⣤⣤⠀⠀⠻⣿⣦⣀⣀⣤⣿⠃⠀⠀⣿⡿⣶⣄⣀⣤⣿⡟⠀⠀⠀⠻⣿⣦⣤⣶⡿⣿⡇⠀⠀⠀⣿⠀⠀⠀⠀⠀⣿⡇⠀⠀⠙⣿⣦⣀⣀⣤⣿⠟⠀⠀⠀⣿⣿⠀⠀⠀⢸⣿⠀⠀⠀⠀',
+      '⠀⠈⠙⠛⠛⠉⠀⠀⠀⠀⠈⠉⠁⠀⠀⠀⠀⠙⠛⠛⠉⠀⠀⠀⠀⠉⠀⠀⠙⠛⠛⠁⠀⠀⠀⠀⠀⠀⠉⠉⠀⠀⠉⠁⠀⠀⠀⠉⠀⠀⠀⠀⠀⠉⠁⠀⠀⠀⠀⠉⠛⠛⠋⠀⠀⠀⠀⠀⠉⠉⠀⠀⠀⠈⠉⠀⠀⠀⠀',
+      '⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀',
+      '⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀',
       '',
     },
     center = {
-      { icon = '  ', desc = 'New file                       ', action = 'enew' },
-      { icon = '  ', shortcut = 'SPC f', desc = 'Find file                 ', action = 'Telescope find_files' },
-      { icon = '  ', shortcut = 'SPC h', desc = 'Recent files              ', action = 'Telescope oldfiles' },
-      { icon = '  ', shortcut = 'SPC g', desc = 'Find Word                 ', action = 'Telescope live_grep' },
+      { icon = '  ', desc = 'New file', action = 'enew' },
+      { icon = '  ', desc = 'Find file               ', key = 'Space + f', action = 'Telescope find_files' },
+      { icon = '\u{f115}  ', desc = 'Recent files            ', key = 'Space + h', action = 'Telescope oldfiles' },
+      { icon = '\u{f1c2}  ', desc = 'Find Word               ', key = 'Space + g', action = 'Telescope live_grep' },
     },
-    footer = {}
+    footer = { '' }
+  },
+  hide = {
+    statusline = false,
+    tabline = false,
+    winbar = false,
   }
 }
 
-vim.cmd([[
-  augroup DashboardHighlights
-    autocmd ColorScheme * highlight DashboardHeader guifg=#6272a4
-    autocmd ColorScheme * highlight DashboardCenter guifg=#f8f8f2
-    autocmd ColorScheme * highlight DashboardShortcut guifg=#bd93f9
-    autocmd ColorScheme * highlight DashboardFooter guifg=#6272a4
-  augroup end
-]])
 vim.api.nvim_set_hl(0, 'DashboardHeader', { fg = '#6272a4' })
-vim.api.nvim_set_hl(0, 'DashboardCenter', { fg = '#f8f8f2' })
-vim.api.nvim_set_hl(0, 'DashboardShortcut', { fg = '#bd93f9' })
+vim.api.nvim_set_hl(0, 'DashboardDesc', { fg = '#f8f8f2' })
+vim.api.nvim_set_hl(0, 'DashboardIcon', { fg = '#bd93f9' })
+vim.api.nvim_set_hl(0, 'DashboardKey', { fg = '#6272a4' })
 vim.api.nvim_set_hl(0, 'DashboardFooter', { fg = '#6272a4' })
--- nvim/lua/user/plugins/lualine.lua
+++ nvim/lua/user/plugins/lualine.lua
@@ -21,7 +21,7 @@ require('lualine').setup({
       'branch',
       'diff',
       separator,
-      '"💻  " .. tostring(#vim.tbl_keys(vim.lsp.buf_get_clients()))',
+      '"\u{f1b3}  " .. tostring(#vim.tbl_keys(vim.lsp.buf_get_clients()))',
       { 'diagnostics', sources = { 'nvim_diagnostic' } },
       separator,
     },
