59563398ed10c7b7cc939f2956f3f91cf0b99ae4
Nick Stebunov <mailbox@stebunoff.ru>
2024-10-11 16:14:33 +0300

    Nvim: decrease terminal height

--- nvim/lua/user/plugins.lua
+++ nvim/lua/user/plugins.lua
@@ -193,7 +193,7 @@ use({
     vim.g.floaterm_width = 0.8
     -- vim.g.floaterm_height = 0.8
     -- Settings for terminal in the bottom
-    vim.g.floaterm_height = 0.4
+    vim.g.floaterm_height = 0.3
     vim.g.floaterm_wintype = 'split'
     vim.keymap.set('n', '<F1>', ':FloatermToggle<CR>')
     vim.keymap.set('t', '<F1>', '<C-\\><C-n>:FloatermToggle<CR>')
