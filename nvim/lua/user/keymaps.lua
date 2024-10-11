d20290803efcf001005f32bdaa61bf3629755d0f
Nick Stebunov <mailbox@stebunoff.ru>
2024-10-11 12:01:34 +0300

    Nvim: fix and add shortcuts

--- nvim/lua/user/keymaps.lua
+++ nvim/lua/user/keymaps.lua
@@ -10,10 +10,19 @@ vim.keymap.set('n', 'j', "v:count == 0 ? 'gj' : 'j'", { expr = true })
 vim.keymap.set('v', '<', '<gv')
 vim.keymap.set('v', '>', '>gv')
 
+-- Maintain the cursor position when yanking a visual selection.
+-- http://ddrscott.github.io/blog/2016/yank-without-jank/
+vim.keymap.set('v', 'y', 'myy`y')
+
+-- Easy insertion of a trailing ; or , from insert mode.
+vim.keymap.set('i', ';;', '<Esc>A;')
+vim.keymap.set('i', ',,', '<Esc>A,')
+
 -- Move text up and down
-vim.keymap.set('i', '<A-j>', '<Esc>:move .+1<CR>==gi')
-vim.keymap.set('i', '<A-k>', '<Esc>:move .-2<CR>==gi')
-vim.keymap.set('n', '<A-j>', ':move .+1<CR>==')
-vim.keymap.set('n', '<A-k>', ':move .-2<CR>==')
-vim.keymap.set('v', '<A-j>', ":move '>+1<CR>gv=gv")
-vim.keymap.set('v', '<A-k>', ":move '<-2<CR>gv=gv")
+-- Use British keyboard on macOS
+vim.keymap.set('i', '∆', '<Esc>:move .+1<CR>==gi')
+vim.keymap.set('i', '˚', '<Esc>:move .-2<CR>==gi')
+vim.keymap.set('n', '∆', ':move .+1<CR>==')
+vim.keymap.set('n', '˚', ':move .-2<CR>==')
+vim.keymap.set('v', '∆', ":move '>+1<CR>gv=gv")
+vim.keymap.set('v', '˚', ":move '<-2<CR>gv=gv")
