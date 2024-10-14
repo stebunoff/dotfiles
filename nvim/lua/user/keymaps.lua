bf8f0a6d8f664b1be967cadb90ec432d6786f7a2
Nick Stebunov <mailbox@stebunoff.ru>
2024-10-14 16:33:22 +0300

    Nvim: fix keymaps to move the lines

--- nvim/lua/user/keymaps.lua
+++ nvim/lua/user/keymaps.lua
@@ -20,9 +20,9 @@ vim.keymap.set('i', ',,', '<Esc>A,')
 
 -- Move text up and down
 -- Use British keyboard on macOS
-vim.keymap.set('i', '∆', '<Esc>:move .+1<CR>==gi')
-vim.keymap.set('i', '˚', '<Esc>:move .-2<CR>==gi')
-vim.keymap.set('n', '∆', ':move .+1<CR>==')
-vim.keymap.set('n', '˚', ':move .-2<CR>==')
-vim.keymap.set('v', '∆', ":move '>+1<CR>gv=gv")
-vim.keymap.set('v', '˚', ":move '<-2<CR>gv=gv")
+vim.keymap.set('i', '<A-j>', '<Esc>:move .+1<CR>==gi')
+vim.keymap.set('i', '<A-k>', '<Esc>:move .-2<CR>==gi')
+vim.keymap.set('n', '<A-j>', ':move .+1<CR>==')
+vim.keymap.set('n', '<A-k>', ':move .-2<CR>==')
+vim.keymap.set('v', '<A-j>', ":move '>+1<CR>gv=gv")
+vim.keymap.set('v', '<A-k>', ":move '<-2<CR>gv=gv")
