d8db7a8194e14eb58a81b1749379246ff23d8930
Nick Stebunov <mailbox@stebunoff.ru>
2025-10-13 14:43:15 +0300

    nvim: fix treesitter issues

--- nvim/lua/user/plugins/treesitter.lua
+++ nvim/lua/user/plugins/treesitter.lua
@@ -1,11 +1,9 @@
-require('ts_context_commentstring').setup({
+require('nvim-treesitter.configs').setup({
   ensure_installed = 'all',
+  ignore_install = { "ipkg" },
   highlight = {
     enable = true,
     additional_vim_regex_highlighting = true,
-  },
-  context_commentstring = {
-    enable = true,
   },
   textobjects = {
     select = {
